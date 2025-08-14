dc5d707f40aa8c3dc3ba8c13de9b107b14051993
Suprasamol Tuppiree <suprasamol@gmail.com>
2025-08-14 22:19:25 +0700

    Update to Page 89

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 e-Book Edwards A. - Let's Go - Learn to Build Professional Web App 2025.pdf
 
-Continue Page. 67
+Continue Page. 90
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,7 +13,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/home.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
 	}
 
 	ts, err := template.ParseFiles(files...)
