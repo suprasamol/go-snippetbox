17cd388bf010ea252fed281f44d3d7a2f8046fcd
Suprasamol Tuppiree <suprasamol@gmail.com>
2025-08-24 15:42:15 +0700

    change package main to model  in snippets.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,12 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql" // New import
+	"github.com/suprasamol/go-snippetbox/internal/models"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
