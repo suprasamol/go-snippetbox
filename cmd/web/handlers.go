b357d50b1774244aa093fbbf5d768a312ff16ed3
Suprasamol Tuppiree <suprasamol@gmail.com>
2026-01-16 23:25:39 +0700

    Finish chapter 5.5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,9 +18,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "home.tmpl.html", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w, r, http.StatusOK, "home.tmpl.html", data)
 
 	// Comment หลังจากเรียกหน้าเว็บผ่าน Cache
 	// files := []string{
@@ -67,9 +68,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "view.tmpl.html", templateData{
-		Snippet: snippet,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.tmpl.html", data)
 
 	// Comment หลังจากเรียกหน้าเว็บผ่าน Cache
 	// files := []string{
