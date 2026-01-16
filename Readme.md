ba1db2eef734f538ff0192da1311dfa43d7c13e7
Suprasamol Tuppiree <suprasamol@gmail.com>
2026-01-16 23:18:45 +0700

    Finish chapter 5.4

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 e-Book Edwards A. - Let's Go - Learn to Build Professional Web App 2025.pdf
 
-Continue Page. 159
+Continue Page. 165
 
 password mysql = root
--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -25,15 +26,20 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
 	ts, ok := app.templateCache[page]
 	if !ok {
-		err := fmt.Errorf("the template %s dose not exist", page)
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
 	w.WriteHeader(status)
 
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
+	buf.WriteTo(w)
 }
