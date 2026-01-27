a164e1ebd0256334c4c3d4b13cbd4ffdd6464005
Suprasamol Tuppiree <suprasamol@gmail.com>
2026-01-27 17:26:28 +0700

    Finish chapter 5

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 e-Book Edwards A. - Let's Go - Learn to Build Professional Web App 2025.pdf
 
-Continue Page. 169
+Continue Page. 175
 
 password mysql = root
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/suprasamol/go-snippetbox/internal/models"
 )
@@ -11,6 +12,17 @@ type templateData struct {
 	CurrentYear int
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	// return t.Format("02 Jan 2006 as 15:04")
+	return t.Format("02/01/2006 15:04")
+	// reference time: Mon Jan 2 15:04:05 MST 2006”
+	// 						1  2  3  4  5       6
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -30,7 +42,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// 	page,
 		// }
 
-		ts, err := template.ParseFiles("./ui/html/Base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/Base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
